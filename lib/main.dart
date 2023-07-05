93ad2fb7b49258f89af2b13d24726d181d650d9f
shinnkura <n.shinzo1010@icloud.com>
2023-07-06 00:18:19 +0900

    画像を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/screens/main/main_screen.dart';
 // import 'package:flutter_portfolio/screens/home/home_screen.dart';
-// import 'package:flutter_portfolio/screens/main/main_screen.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
 
@@ -10,24 +10,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      // we are using dark theme and we modify it as our need
       theme: ThemeData.dark().copyWith(
         primaryColor: primaryColor,
-        // scaffoldBackgroundColor: bgColor,
-        // canvasColor: bgColor,
-        // textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-        //     .apply(bodyColor: Colors.white)
-        //     .copyWith(
-        //       bodyText1: TextStyle(color: bodyTextColor),
-        //       bodyText2: TextStyle(color: bodyTextColor),
-        //     ),
+        scaffoldBackgroundColor: bgColor,
+        canvasColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white)
+            .copyWith(
+              bodyLarge: TextStyle(color: bodyTextColor),
+              bodyMedium: TextStyle(color: bodyTextColor),
+            ),
       ),
+      home: MainScreen(),
       // home: HomeScreen(),
     );
   }
